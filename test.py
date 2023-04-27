ff3d484ee6ded842932228499aa66b376c50ead6
knuos <112151897+knuos@users.noreply.github.com>
2023-04-27 19:16:28 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
+def right_triangle(a, b, c): 
 # Write python code below
-s = input()
+
+
+
+
+if right_triangle(a, b, c):
+  print(a, ', ', b, ', ', c,' 로 만들어진 삼각형은 직각삼각형입니다')
+else:
+  print(a, ', ', b, ', ', c,' 로 만들어진 삼각형은 직각삼각형이 아닙니다')
