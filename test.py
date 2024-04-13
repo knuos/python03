d49fd58c890d8b068be7b3ef0e46b20e30f88cc4
knuos <112151897+knuos@users.noreply.github.com>
2024-04-13 13:42:21 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
-def right_triangle(a, b, c): 
+def cal_avg(num): 
 # Write python code below
 
 
 
 
-if right_triangle(a, b, c):
-  print(a, ',', b, ',', c, '로 만들어진 삼각형은 직각삼각형입니다.')
-else:
-  print(a, ',', b, ',', c, '로 만들어진 삼각형은 직각삼각형이 아닙니다.')
